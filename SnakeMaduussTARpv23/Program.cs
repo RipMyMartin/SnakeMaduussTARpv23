326534be4db19fb6d56dc5c71c4a13e7c9ac49d4
My Rip <ripmyloven@gmail.com>
2024-09-09 13:41:45 +0300

    Loeng Nr6

--- SnakeMaduussTARpv23/Program.cs
+++ SnakeMaduussTARpv23/Program.cs
@@ -17,13 +17,27 @@ namespace SnakeMaduussTARpv23
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Console.ReadLine();
-        }
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+            int x = numList[ 0 ];
+            int y = numList[ 1 ];
+            int z = numList[ 2 ];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine( i );
+            }
+            numList.Remove( 0 );
+            
+            List<Point> plist = new List<Point>();
+
+            plist.Add( p1 );
+            plist.Add( p2 );
+
+            Console.ReadLine();
         }
     }
 }
